b01d44872291b1d049666b345f979f09112a4e64
RixTheTyrunt <87478581+RixInGithub@users.noreply.github.com>
2022-06-13 13:13:26 +0000

    Initial commit

---
+++ test.py
@@ -0,0 +1,6 @@
+# bro
+print "Is it invalid?"
+print "Who knows!"
+print "Maybe it is, maybe it's not..."
+print("Maybe with a parenthesis?")
+print("I think it doesn't look like a Pythonic way to use paranthesis...")
